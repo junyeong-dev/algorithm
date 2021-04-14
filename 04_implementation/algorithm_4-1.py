e9d3db4340315396f9f7c740aa85366d1fe4775b
Jun-cloud <kjy8827@gmail.com>
2021-04-14 17:32:51 +0900

    algorithm 4-1 - Supplementary answer

--- 04_implementation/algorithm_4-1.py
+++ 04_implementation/algorithm_4-1.py
@@ -23,3 +23,23 @@ for i in move:
         y = tempY
 
 print(x, y)
+
+# 답안 예시
+n = int(input())
+plans = input().split()
+x, y = 1, 1
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+moveTypes = ['L', 'R', 'U', 'D']
+
+for plan in plans:
+    for i in range(len(moveTypes)):
+        if plan == moveTypes[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
