87e96da1e87223e75a2bc183b7e7631b958a7e05
Jun-cloud <kjy8827@gmail.com>
2021-04-16 12:13:23 +0900

    algorithm 4-2

--- 04_implementation/algorithm_4-1.py
+++ 04_implementation/algorithm_4-1.py
@@ -15,50 +15,50 @@
 # 출력 예시
 # 3 4
 
-# # 공간의 크기
-# n = int(input())
-# # 이동 계획
-# move = input().split()
-# x, y = 1, 1
-#
-# for i in move:
-#     tempX = x
-#     tempY = y
-#     if i == "R":
-#         tempY += 1
-#     elif i == "L":
-#         tempY -= 1
-#     elif i == "D":
-#         tempX += 1
-#     elif i == "U":
-#         tempX -= 1
-#
-#     if 0 < tempX <= n:
-#         x = tempX
-#     if 0 < tempY <= n:
-#         y = tempY
-#
-# print(x, y)
-#
-# # 답안 예시
-# n = int(input())
-# plans = input().split()
-# x, y = 1, 1
-#
-# dx = [0, 0, -1, 1]
-# dy = [-1, 1, 0, 0]
-# moveTypes = ['L', 'R', 'U', 'D']
-#
-# for plan in plans:
-#     for i in range(len(moveTypes)):
-#         if plan == moveTypes[i]:
-#             nx = x + dx[i]
-#             ny = y + dy[i]
-#     if nx < 1 or ny < 1 or nx > n or ny > n:
-#         continue
-#     x, y = nx, ny
-#
-# print(x, y)
+# 공간의 크기
+n = int(input())
+# 이동 계획
+move = input().split()
+x, y = 1, 1
+
+for i in move:
+    tempX = x
+    tempY = y
+    if i == "R":
+        tempY += 1
+    elif i == "L":
+        tempY -= 1
+    elif i == "D":
+        tempX += 1
+    elif i == "U":
+        tempX -= 1
+
+    if 0 < tempX <= n:
+        x = tempX
+    if 0 < tempY <= n:
+        y = tempY
+
+print(x, y)
+
+# 답안 예시
+n = int(input())
+plans = input().split()
+x, y = 1, 1
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+moveTypes = ['L', 'R', 'U', 'D']
+
+for plan in plans:
+    for i in range(len(moveTypes)):
+        if plan == moveTypes[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
 
 # 예제 4-1-2
 # 정수 n이 입력되면 00시 00분 00초부터 n시 59분 59초까지의 모든 시각 중에서 3이 하나라도 포함되는 모든
