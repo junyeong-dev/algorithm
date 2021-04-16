7dbd7f33d17f4ef92734d887a0f95f1d6fc7a14d
Jun-cloud <kjy8827@gmail.com>
2021-04-16 12:41:08 +0900

    algorithm 4-2 - Supplementary answer

--- 04_implementation/algorithm_4-2.py
+++ 04_implementation/algorithm_4-2.py
@@ -32,3 +32,19 @@ for root in roots:
         result += 1
 
 print(result)
+
+##### 답안 예시
+input_data = input()
+row = int(input_data[1])
+# ord()는 문자의 유니코드 값을 돌려주는 함수이다.
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+result = 0
+
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+
+print(result)
