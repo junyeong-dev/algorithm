3fd855807f8d3fb34c949929ebb37374529fd275
Jun-cloud <kjy8827@gmail.com>
2021-04-18 12:49:48 +0900

    algorithm 5-1-1

---
+++ DFS_BFS/algorithm_5-1-1.py
@@ -0,0 +1,17 @@
+# 스택 Stack
+# 선입후출 또는 후입선출구조라고 함
+# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+stack = []
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+# 최하단 원소부터 출력
+print(stack)
+# 최상단 원소부터 출력
+print(stack[::-1])
