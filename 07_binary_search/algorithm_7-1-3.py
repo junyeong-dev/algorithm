e231295597bb8e9c0c4241ce1f5877ce7c35b933
Jun-cloud <kjy8827@gmail.com>
2021-05-17 20:53:19 +0900

    algorithm 7-1-3

---
+++ 07_binary_search/algorithm_7-1-3.py
@@ -0,0 +1,20 @@
+# 반복문으로 구현한 이진 탐색
+def binary_search(array, target, start, end):
+
+    while True:
+        mid = (start + end) // 2
+        if start > end:
+            return print('원소가 존재하지 안습니다.')
+        if array[mid] > target:
+            end = mid - 1
+        elif array[mid] < target:
+            start = mid + 1
+        else:
+            return print(mid)
+
+# n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
+n, target = list(map(int, input().split()))
+# 전체 원소 입력받기
+array = list(map(int, input().split()))
+
+binary_search(array, target, 0, n - 1)
