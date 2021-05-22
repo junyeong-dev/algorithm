fa633dcdd086d19a293dd48f6b88b40c7870ac9c
Jun-cloud <kjy8827@gmail.com>
2021-05-22 22:32:11 +0900

    algorithm 7-3 - Supplementary answer

--- 07_binary_search/algorithm_7-3.py
+++ 07_binary_search/algorithm_7-3.py
@@ -12,20 +12,49 @@
 # 출력 예시
 # 15
 
+# 떡의 개수(n)와 요청한 떡의 길이(m)을 입력받기
 n, m = list(map(int, input().split()))
+# 각 떡의 개별 높이 정보를 입력받기
 array = list(map(int, input().split()))
 
-h = max(array) - 1
-while True:
-    rice_cake = 0
-    for i in array:
-        if i - h < 0:
-            rice_cake += 0
-        else:
-            rice_cake += i - h
-    if rice_cake < m:
-        h -= 1
-    else:
-        break
+# h = max(array) - 1
+# while True:
+#     rice_cake = 0
+#     for i in array:
+#         if i - h < 0:
+#             rice_cake += 0
+#         else:
+#             rice_cake += i - h
+#     if rice_cake < m:
+#         h -= 1
+#     else:
+#         break
+#
+# print(h)
+# >> 탐색 범위의 따라 시간 초과 가능성이 높다
+# 그러므로 이진 탐색으로 해결해야 함
 
-print(h)
+# 답변 예시
+# 이진 탐색을 위한 시작점과 끝점 설정
+start = 0
+end = max(array)
+
+# 이진 탐색
+result = 0
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+        # 잘랐을 떄의 떡의 양 계산
+        if x > mid:
+            total += x - mid
+    # 떡의 양이 부족한 경우 더 많이 자르기(왼쪽 부분 탐색)
+    if total < m:
+        end = mid - 1
+    # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
+    else:
+        # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록
+        result = mid
+        start += 1
+
+print(result)
