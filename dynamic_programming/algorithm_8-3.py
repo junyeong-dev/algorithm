fa6661ad7e2dc9c6e82a3f930d2e89d206619375
Jun-cloud <kjy8827@gmail.com>
2021-06-03 10:11:45 +0900

    algorithm 8-3

--- dynamic_programming/algorithm_8-3.py
+++ dynamic_programming/algorithm_8-3.py
@@ -14,3 +14,15 @@
 
 n = int(input())
 warehouse = list(map(int, input().split()))
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d = [0] * 100
+
+# 다이나믹 프로그래밍(Dynamic Programing) 진행(보텀업)
+d[0] = warehouse[0]
+d[1] = max(warehouse[0], warehouse[1])
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + warehouse[i])
+
+# 계산된 결과 출력
+print(d[n - 1])
