05d704d8dab7335b460a572a8fd1a967835087b6
Jun-cloud <kjy8827@gmail.com>
2021-06-10 22:09:43 +0900

    algorithm 8-5

--- dynamic_programming/algorithm_8-5.py
+++ dynamic_programming/algorithm_8-5.py
@@ -19,10 +19,26 @@
 # 출력 예시 2
 # -1
 
+#
+
 n, m = map(int, input().split())
 
 array = []
 for i in range(n):
     array.append(int(input()))
 
-d = [0] * 10000
+# 한 번 계산된 결과를 저장하기 위한 DP 체이블 초기화
+d = [10001] * (m + 1)
+
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(버텀업)
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001:
+            d[j] = min(d[j], d[j - array[i]] + 1)
+
+# 계산된 결과 출력
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
